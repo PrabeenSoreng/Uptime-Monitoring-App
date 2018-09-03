9eda2d72ba83fec71877acc9452df26ffa632548
prabeen <prabeensoreng2119@gmail.com>
2018-09-03 20:26:37 +0530

    adding log related worker

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const workers = require('./lib/workers');
 var app = {};
 
 app.init = function() {
+    console.log('\x1b[36m%s\x1b[0m', 'Background workers are running...');
     server.init();
     workers.init();
 };
